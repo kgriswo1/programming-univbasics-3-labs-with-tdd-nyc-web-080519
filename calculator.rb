a6cbe7916ea7942ef20d2889281fec9e0de7bb0b
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-17 15:13:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,6 @@
 first_number = 0
 second_number = 8
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
